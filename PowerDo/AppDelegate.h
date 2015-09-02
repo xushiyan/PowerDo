0a0d585c83a603805943825b692fabaf95ad8a9f
XU SHIYAN <xushiyan.1990@gmail.com>
2015-09-02 22:14:14 +0800

    Project setup

---
+++ PowerDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerDo
+//
+//  Created by XU SHIYAN on 9/2/15.
+//  Copyright © 2015 xushiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PowerDo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PowerDo
+//
+//  Created by XU SHIYAN on 9/2/15.
+//  Copyright © 2015 xushiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
