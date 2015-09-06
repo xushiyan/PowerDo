ee8a4ab15ee21674ba5a42ebbbae6afff1cc61bd
XU SHIYAN <xushiyan.1990@gmail.com>
2015-09-06 15:42:43 +0800

    Setup core data stack

--- PowerDo/AppDelegate.h
+++ PowerDo/AppDelegate.h
@@ -8,10 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+@class PWDTaskManager;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) PWDTaskManager *taskManager;
 
 @end
 
