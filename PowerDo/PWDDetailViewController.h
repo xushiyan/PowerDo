466836a4dffbca4906072f9c2198af9bd242cf63
XU SHIYAN <xushiyan.1990@gmail.com>
2015-11-24 13:04:18 +0800

    created detail view for task

---
+++ PowerDo/PWDDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PWDDetailViewController.h
+//  PowerDo
+//
+//  Created by Xu, Raymond on 11/24/15.
+//  Copyright © 2015 xushiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWDDetailViewController : UIViewController
+
+@end
